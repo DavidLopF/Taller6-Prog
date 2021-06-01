4940ba6781be97020d5b4b98c541e3d1e30fd7a4
Sergio-1603 <sergioeduardo2000@gmail.com>
2021-05-31 21:09:45 -0500

    Agregando mascotas a los duenos

--- src/main/java/co/edu/unbosque/Taller6_Prog/resources/OwnersResource.java
+++ src/main/java/co/edu/unbosque/Taller6_Prog/resources/OwnersResource.java
@@ -2,6 +2,7 @@ package co.edu.unbosque.Taller6_Prog.resources;
 
 import co.edu.unbosque.Taller6_Prog.resources.Pojos.Notification;
 import co.edu.unbosque.Taller6_Prog.resources.Pojos.Owner;
+import co.edu.unbosque.Taller6_Prog.resources.Pojos.Pet;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -31,6 +32,15 @@ public class OwnersResource {
         owners.add(new Owner("Usuario2", 2, "Chad", "address2", "Usaquen"));
         owners.add(new Owner("Usuario3", 3, "Piter", "address3", "Usaquen"));
         owners.add(new Owner("Usuario4", 1, "Jackson", "address4", "Usaquen"));
+
+        owners.get(0).addPet(new Pet(1,2,"Pet1","specie1","race1","size1","macho",owners.get(0).getPerson_id()));
+        owners.get(0).addPet(new Pet(2,2,"Pet2","specie2","race2","size2","hembra",owners.get(0).getPerson_id()));
+        owners.get(0).addPet(new Pet(3,2,"Pet3","specie3","race3","size3","macho",owners.get(0).getPerson_id()));
+
+        owners.get(3).addPet(new Pet(4,2,"Pet4","specie4","race4","size4","hembra",owners.get(3).getPerson_id()));
+        owners.get(3).addPet(new Pet(5,2,"Pet5","specie5","race5","size5","macho",owners.get(3).getPerson_id()));
+
+
 
         for (Owner o : owners) {
             if (filtro.equalsIgnoreCase("identificacion")) {
