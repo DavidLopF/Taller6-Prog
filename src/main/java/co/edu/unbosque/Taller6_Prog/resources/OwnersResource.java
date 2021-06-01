4b242c24b4beb4f2c4651d2b6e7656ac765b2fbc
Briannys Monserrate <bpaezm@unbosque.edu.co>
2021-05-31 20:20:13 -0500

    ger owners iniciado

--- src/main/java/co/edu/unbosque/Taller6_Prog/resources/OwnersResource.java
+++ src/main/java/co/edu/unbosque/Taller6_Prog/resources/OwnersResource.java
@@ -6,16 +6,20 @@ import co.edu.unbosque.Taller6_Prog.resources.Pojos.Owner;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/owners")
 public class OwnersResource {
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response create(Owner owner) {
-        owner.setPerson_id(3);
+    public Response list() {
+        List<Owner> owners = new ArrayList<Owner>();
+        owners.add(new Owner("brix",1, "briannysm", "calle 147", "usaquen"));
+        owners.add(new Owner("brisita",2, "brimonse", "calle 147", "cedritos"));
+
         return Response.status(Response.Status.CREATED)
-                .entity(owner)
+                .entity(owners)
                 .build();
     }
 }
